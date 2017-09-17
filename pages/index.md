9502bbc8c4e98c8d2cbc099393d375201e10383d
Daniel Winterstein <daniel@sodash.com>
2017-09-17 09:15:27 +0100

    cleaner

--- pages/index.md
+++ pages/index.md
@@ -1,6 +1,8 @@
 banner1: My-Loop: Your Data, Under Your Control.
 banner2: A safe and ethical data collection, management, and monetisation tool
 
+<a name='about'></a>
+
 <div class="row">
 <div class="col-md-6">
 
@@ -35,13 +37,92 @@ My-Loop is a better way: we replace
 covert surveillance with honest partnership, 
 making user-control an asset not an inconvenience.
 
+My-Loop provides transparency and an ethical approach 
+to using profiling and customer data in 
+advertising, marketing, and sales.
+
 <img src="img/cameras.on.wall.jpg" class="img-fit">
-
-My-Loop will allow users to control how their data is used by third-party researchers and marketers.  Allowing for more transparency and an ethical approach to marketing/advertising.
-
 
 <div class="old-radio" id="objectives">
 
 # Our objectives
+
+1. Personal Control: 
+The public should have control over their personal data.
+GDPR will soon make this a legal requirement in the UK and
+EU with serious financial penalties. 
+However implementing GDPR compliant sales & marketing is a challenge.
+
+2. Simplicity: 
+Provide a simple, effortless and safe way for the general public to see, share and monetise their personal data
+
+3. Value: To harness the value of quality data for business.
+
+4. Transparency: Bring data capture and usage out of
+the dark.
+
+5. Quality: helping organisations access a greater quantity and quality of data, while complying with GDPR legal rules.
+
+</div>
+
+<div>
+
+## How it Works
+
+### For Business
+
+Upload data into My-Loop
+
+Access via our simple API
+
+Connect to the GDPR actions API to stay compliant
+
+
+Monetise your data
+
+Positive experience
+
+My-Loop creates a positive online experience where consumers can take back control and fund real social impact. In addition it breathes new life into brand engagement online as low-value banner space is transformed into high-value targeted video advertising. Directly empowering the public increases trust and circumvents a common issue with privacy solutions by leaving the definition of acceptable intrusion to the creator. My-Loop seeks not to end the use of personal data for commercial means but to make it more effective and respectful, while helping regulate, inform, and educate
+
+### For Individuals
+
+1. Your My Loop profile
+My-Loop collects data about you from various sources, and organises it into a profile. This is your profile -- you can access and control this data through the secure My Loop web-app. Explore what your digital profile reveals about you. Choose what data is private, and what can be used and by whom. Decide what charities you want to support.
+
+</div>
+
+<div class="col-md-12 ferriswheel-bg" id="contact">
+
+## Contact Us
+
+<form class="contact" action="php/mailer.php" target="contact-iframe" method="post" id="contactform">
+	<div class="col-md-offset-4 col-md-4">
+		<div class="col-md-offset-3 col-md-6">
+			<span class="thankyoufield" id="sendclickresult"></span>
+		</div>
+		<div class="form-group">
+			<input class="col-md-5" type="text" name="name" id="namefield" placeholder="Name" required="">
+			<div class="help-block with-errors">
+			</div>
+			<div class="col-md-2">
+			</div>
+			<input class="col-md-5" type="email" name="email" id="emailfield" placeholder="Email" required="">
+			<div class="help-block with-errors">
+			</div>
+			<!-- Cheap Vertical Space -->
+			<div class="col-md-12" style="height:1em;">
+			</div>
+			<!-- End of Cheap Vertical Space -->
+			<textarea rows="4" placeholder="Message" required="" class="form-control" id="messagefield" name="message"></textarea>
+			<!-- Cheap Vertical Space -->
+			<div class="col-md-12" style="height:1em;">
+			</div>
+			<!-- End of Cheap Vertical Space -->
+			<center>
+				<button class="jumbotron-btn btn" type="submit" id="sendcontactform">Submit</button>
+			</center>
+		</div>
+	</div>
+</form>        
 
 </div>
--- webroot/js/my-loop.js
+++ webroot/js/my-loop.js
@@ -1,164 +1,42 @@
 // The disappearing navbar
-(function ($) {
-  $(document).ready(function(){
+$(function(){
     
 	// hide .navbar first
 	$(".navbar").hide();
 	
 	// fade in .navbar
-	$(function () {
-		$(window).scroll(function () {
-            // set distance user needs to scroll before we fadeIn navbar
-			if ($(this).scrollTop() > 100) {
-				$('.navbar').fadeIn();
-			} else {
-				$('.navbar').fadeOut();
-			}
-		});
-
-	
+	$(window).scroll(function () {
+		// set distance user needs to scroll before we fadeIn navbar
+		if ($(this).scrollTop() > 100) {
+			$('.navbar').fadeIn();
+		} else {
+			$('.navbar').fadeOut();
+		}
 	});
 
 });
-  }(jQuery));
 // 
 
-// The 'Learn More' Jumbotron Button
-$(document).ready(function (){
-	$('#btn-learnmore').click(function() {
+// Animated scroll-to anchor
+$(function () {
+	$('a').click(function(e) {
+		let href = $(e.target).attr('href');
+		if ( ! href || href[0] !== '#') return;
+		let targeta = $('a[name="'+href+'"]');
+		console.log("scroll-to", href, targeta);
+		if ( ! targeta.length) return;
 		$('html, body').animate( {
-			scrollTop: $("#ourvision").offset().top
-		}, 2000);
+			scrollTop: targeta.offset().top
+		}, 1000);
+		e.preventDefault();
 	});
 });
 // 
-
-//  The Navlink "Home" AKA "My Loop" function
-$(document).ready(function (){
-	$('#navhome').click(function() {
-		$('html, body').animate( {
-			scrollTop: $("#top").offset().top
-		}, 2000);
-	});
-});
-// 
-
-// The Navlink "About" function
-$(document).ready(function (){
-	$('#navabout').click(function() {
-		$('html, body').animate( {
-			scrollTop: $("#ourvision").offset().top
-		}, 2000);
-	});
-});
-// 
-
-// The Navlink "How" function
-$(document).ready(function (){
-	$('#navhow').click(function() {
-		$('html, body').animate( {
-			scrollTop: $("#how").offset().top
-		}, 2000);
-	});
-});
-// 
-
-// The Navlink "Contact Us" function
-$(document).ready(function (){
-	$('#navcontact').click(function() {
-		$('html, body').animate( {
-			scrollTop: $("#contact").offset().top
-		}, 2000);
-	});
-});
-// 
-
-
-//  The UPPER Navlink "Home" AKA "My Loop" function
-$(document).ready(function (){
-	$('#uppernavhome').click(function() {
-		$('html, body').animate( {
-			scrollTop: $("#top").offset().top
-		}, 2000);
-	});
-});
-// 
-
-// The UPPER Navlink "About" function
-$(document).ready(function (){
-	$('#uppernavabout').click(function() {
-		$('html, body').animate( {
-			scrollTop: $("#ourvision").offset().top
-		}, 2000);
-	});
-});
-// 
-
-// The UPPER Navlink "How" function
-$(document).ready(function (){
-	$('#uppernavhow').click(function() {
-		$('html, body').animate( {
-			scrollTop: $("#how").offset().top
-		}, 2000);
-	});
-});
-// 
-
-// The UPPER Navlink "Contact Us" function
-$(document).ready(function (){
-	$('#uppernavcontact').click(function() {
-		$('html, body').animate( {
-			scrollTop: $("#contact").offset().top
-		}, 2000);
-	});
-});
-// 
-
-
-//  The MOBILE Navlink "Home" AKA "My Loop" function
-$(document).ready(function (){
-	$('#mobilenavhome').click(function() {
-		$('html, body').animate( {
-			scrollTop: $("#top").offset().top
-		}, 2000);
-	});
-});
-// 
-
-// The MOBILE Navlink "About" function
-$(document).ready(function (){
-	$('#mobilenavabout').click(function() {
-		$('html, body').animate( {
-			scrollTop: $("#ourvision").offset().top
-		}, 2000);
-	});
-});
-// 
-
-// The MOBILE Navlink "How" function
-$(document).ready(function (){
-	$('#mobilenavhow').click(function() {
-		$('html, body').animate( {
-			scrollTop: $("#how").offset().top
-		}, 2000);
-	});
-});
-// 
-
-// The MOBILE Navlink "Contact Us" function
-$(document).ready(function (){
-	$('#mobilenavcontact').click(function() {
-		$('html, body').animate( {
-			scrollTop: $("#contact").offset().top
-		}, 2000);
-	});
-});
-// 
-
 
 
 // ContactUs formsubmission creates a "thank you" section
-$(document).ready(function (){
+// TODO replace with a My-Loop form
+$(function (){
 	$('#sendcontactform').click(function () {
 		$('#sendclickresult').empty().html("Thank you for your interest in My-Loop");
 		var delay=5000; // 5 seconds
