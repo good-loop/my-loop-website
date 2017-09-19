8ec8dce3328871fdb50abe2d2bf4bf275a332914
DA <daniel.appel.winterwell@gmail.com>
2017-09-19 10:56:49 +0100

    fixed the centering of the footer and the text-decoration on the footer href's

--- render-html.sh
+++ render-html.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -cp ~/winterwell/jerbil/jerbil.jar:lib/* Jerbil ~/winterwell/good-loop/my-loop-website
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -cp /home/daniel/winterwell/jerbil/jerbil.jar:/home/daniel/winterwell/jerbil/lib/* Jerbil /home/daniel/winterwell/good-loop/my-loop-website
